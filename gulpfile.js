2c0335c391c3553bb5b551fd874b408f07be1521
Fredi <koolbody@web.de>
2017-02-26 21:15:04 +0100

    gulp runs now both task after each other, updated package.json with concatCSS

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,16 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     concatCss = require('gulp-concat-css');
 
-gulp.task('default', function(){
-});
+gulp.task('default', ['js', 'css']);
 
-gulp.task('build', function(){
+gulp.task('js', function(){
     return gulp.src(['./distr/dependencies/jquery.min.js', './distr/dependencies/jazz.min.js', './distr/dependencies/pagedown.min.js','./distr/sideshow.js','./tutorial/scripts/sideshow.config.js'])
     .pipe(concat('build.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('css', function () {
+gulp.task('css',['js'], function () {
     return gulp.src(['./distr/stylesheets/sideshow.min.css', './distr/fonts/sideshow-fontface.min.css'])
     .pipe(minifyCss())
     .pipe(concatCss("./bundle.css"))
