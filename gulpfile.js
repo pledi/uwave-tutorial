495f7bbe8c0f0fe02b596d8301d9688ac25b48a6
Fredi <koolbody@web.de>
2017-02-26 20:18:59 +0100

    fixed gulpfile, now concat in the right order

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('default', function(){
 });
 
 gulp.task('build', function(){
-    gulp.src('./distr/*.js')
+    return gulp.src(['./distr/dependencies/jquery.min.js', './distr/dependencies/jazz.min.js', './distr/dependencies/pagedown.min.js','./distr/sideshow.js','./tutorial/scripts/sideshow.config.js'])
     .pipe(concat('build.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./'));
