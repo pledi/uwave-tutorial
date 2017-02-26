7f736d56ec6de6f9b98dca10bfe9e05f340bef9b
Fredi <koolbody@web.de>
2017-02-26 20:43:37 +0100

    bundle css in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minifyCss = require("gulp-minify-css"),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css');
 
 gulp.task('default', function(){
 });
@@ -10,5 +11,12 @@ gulp.task('build', function(){
     return gulp.src(['./distr/dependencies/jquery.min.js', './distr/dependencies/jazz.min.js', './distr/dependencies/pagedown.min.js','./distr/sideshow.js','./tutorial/scripts/sideshow.config.js'])
     .pipe(concat('build.js'))
     .pipe(uglify())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('css', function () {
+    return gulp.src(['./distr/stylesheets/sideshow.min.css', './distr/fonts/sideshow-fontface.min.css'])
+    .pipe(minifyCss())
+    .pipe(concatCss("./bundle.css"))
     .pipe(gulp.dest('./'));
 });
